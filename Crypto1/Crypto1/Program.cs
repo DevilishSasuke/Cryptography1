21c5b7b797efe5e9ab4426075ba3e62cd3b3c9f2
DevilishSasuke <108042884+DevilishSasuke@users.noreply.github.com>
2024-02-21 23:53:50 +0300

    init

---
+++ Crypto1/Crypto1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Numerics;
+
+namespace Cryptography
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            CryptoRSA unit = new CryptoRSA();
+            string answer = "";
+
+            while (true)
+            {
+                Console.WriteLine("\n-----------------\n");
+                Console.WriteLine("Выберите опцию:");
+                Console.WriteLine("1.Сгенерировать новые ключи");
+                Console.WriteLine("2.Зашифровать текст");
+                Console.WriteLine("3.Расшифровать текст");
+                Console.WriteLine("4. Выход");
+
+                do answer = Console.ReadLine();
+                while (answer != "1" && answer != "2" &&
+                    answer != "3" && answer != "4");
+
+                switch (answer)
+                {
+                    case "1":
+                        unit.GenerateNewKeys();
+                        Console.WriteLine(String.Format("\np: {0}; \nq: {1}; \ne: {2}; \nd: {3};",
+                            unit.p, unit.q, unit.e, unit.d));
+                        break;
+                    case "2":
+                        Console.WriteLine("\nВведите текст для шифрования: ");
+                        var encrypted = unit.EncryptText(GetNumberFromConsole());
+                        Console.WriteLine("Encrypted text:\n" + encrypted);
+                        break;
+                    case "3":
+                        Console.WriteLine("\nВведите текст для расшифровки: ");
+                        var decrypted = unit.DecryptText(GetNumberFromConsole());
+                        Console.WriteLine("Decrypted text:\n" + decrypted);
+                        break;
+                    case "4":
+                        return;
+                }
+            }
+        }
+
+
+        public static string ChooseE()
+        {
+            string answer = "";
+            while (answer != "1" && answer != "2")
+            {
+                Console.WriteLine("\n1. Ввести e вручную");
+                Console.WriteLine("2. Вычислить e");
+                answer = Console.ReadLine();
+            }
+
+            if (answer == "1")
+                Console.WriteLine("Введите значение e: ");
+
+            return answer;
+        }
+
+        public static BigInteger GetNumberFromConsole()
+        {
+            BigInteger result;
+
+            while (!BigInteger.TryParse(Console.ReadLine(), out result)) { }
+            return result;
+        }
+    }
+
+    
+}
